77b0b3793451fb2c0380f3af74bd65ed7a60d4b3
Iris14159 <ampheriz0@gmail.com>
2023-11-22 18:08:36 -0600

    connect

--- connect.js
+++ connect.js
@@ -1,6 +1,15 @@
+/*----------------------------------------------------------
+ * connect.js
+ *
+ * Date: 21-Nov-2023
+ * Authors:
+ *           A01799387 Renato Garcia Moran
+ *           A01798048 Maximilisno De La Cruz Lima
+ *           A01798199 Fidel Alexander Bonilla Montalvo
+ *----------------------------------------------------------*/
 let mysql = require("mysql");
 
-const databaseName = "classicmodels";
+const databaseName = "retofinal";
 
 let connection = mysql.createConnection({
   host: "localhost",
